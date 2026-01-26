1b53213a4d6c761acbc22e52747820edbc3bae30
Ediii Dev <ediii.dev@gmail.com>
2026-01-26 10:35:31 -0400

    Update about personal data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,87 +1,72 @@
 export const personalInfo = {
-  name: "Lorem Ipsum",
-  location: "Dolor Sit Amet",
-  email: "lorem@ipsum.com",
-  github: "https://github.com/loremipsum",
-  linkedin: "https://linkedin.com/in/loremipsum",
+  name: "Ediliani Kumquel",
+  location: "Caracas - Venezuela",
+  //email: "lorem@ipsum.com",
+  github: "https://github.com/ediiidev",
+  linkedin: "https://www.linkedin.com/in/ediliani-kumquel-358043203",
   profilePicture: "/profile.jpeg",
   heroDescription:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    "Soy Ediliani Kumquel, Ingeniera de Sistemas egresada de la Universidad Nacional Experimental Politécnica 'Antonio José de Sucre' Vicerrectorado 'Luis Caballero Mejías', donde adquirí conocimientos de diseño, programación, análisis y gestión de sistemas informáticos. Además, tengo varias certificaciones de LinkedIn en desarrollo web full-stack, que me han permitido desarrollar aplicaciones web dinámicas y escalables. Me considero una persona proactiva, creativa, colaborativa y comprometida con la excelencia y el aprendizaje continuo. Mi objetivo es seguir creciendo como profesional y como persona, y aportar mi experiencia y conocimientos al éxito de los proyectos en que participo. Entre mis hobbies están el running, bailar, la música, las manualidades y muchos más.",
 };
 
 export const workExperience = [
   {
-    company: "Lorem Ipsum Corp",
-    location: "Lorem City",
-    position: "Lorem Engineer",
-    period: "Jan 2020 - Present",
+    company: "Sybven",
+    location: "Caracas Venezuela",
+    position: "Especilista de Sistemas",
+    period: "Mar. 2025 - Nov. 2025",
     achievements: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco.",
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse.",
-      "Excepteur sint occaecat cupidatat non proident.",
-      "Sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.",
+      "Especialista de las plataformas DANAConnect y Kore.ai",
+      "Apoyo técnico en sesiones de preventa y postventa.",
+      "Desarrollo de propuestas técnicas.",
+      "Desarrollo de capacitaciones para equipos funcionales y técnicos.",
+      "Desarrollo de POCs.",
+      "Desarrollo de DEMOS.",
+      "Desarrollo de RFPs.",
     ],
   },
   {
-    company: "Dolor Sit Amet",
-    location: "Ipsumville",
-    position: "Software Developer",
-    period: "Feb 2019 - Jan 2020",
+    company: "Sybven",
+    location: "Caracas Venezuela",
+    position: "Consultor Junior",
+    period: "ago. 2021 - feb 2025",
     achievements: [
-      "Morbi in sem quis dui placerat ornare.",
-      "Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam.",
+      "Experiencia con los lenguajes HTML5, CSS3, Javascript, framework Visualizer.",
+      "Desarrollo de aplicaciones Web y Móviles, experiencia en el sector bancario.",
+      "Identificación y corrección de errores del aplicativo web y móvil.",
+      "Desarrollo de documentación técnica.",
+      "Pruebas Unitarias.",
     ],
-  },
-  {
-    company: "Consectetur Inc.",
-    location: "Adipiscing, Ipsum",
-    position: "Intern",
-    period: "Jun 2018 - Feb 2019",
-    achievements: [
-      "Praesent dapibus, neque id cursus faucibus.",
-      "Fusce feugiat malesuada odio.",
-    ],
-  },
+  }
 ];
 
 export const education = [
   {
-    institution: "Lorem Ipsum University",
-    location: "Lorem City",
-    degree: "BSc Lorem Ipsum",
-    period: "2015 - 2019",
+    institution: "Universidad Nacional Experimental Politécnica \"Antonio José de Sucre\" Vicerrectorado \"Luis Caballero Mejías\"",
+    location: "Caracas Venezuela",
+    degree: "Ingeniero de Sistemas",
+    period: "2015 - 2023",
     achievements: [
-      "President of Lorem Ipsum Club",
-      "Organized multiple lorem events",
-      "Represented university in national competitions.",
-      "Graduated with honors.",
+      "Graduado con Honores: ",
+      "Indice de rendimiento Uno.",
+      "Segundo Mejor Indice Academico de la promoción y de la especialidad de Sistemas.",
     ],
   },
 ];
 export const skills = {
   programmingLanguages: [
-    "LoremLang",
-    "IpsumScript",
-    "DolorLang",
-    "SitLang",
-    "AmetLang",
-    "ConsecteturLang",
+    "JavaScript",
+    "Python",
+    "n8n",
   ],
   frontendDevelopment: [
-    "LoremJS",
-    "IpsumJS",
-    "Dolor Native",
-    "Sit UI",
-    "Amet CSS",
-    "HTML",
-    "CSS",
+    "HTML5",
+    "CSS3",
+    "React",
   ],
-  backendDevelopment: ["LoremNode", "IpsumExpress"],
-  databaseAndStorage: ["LoremDB", "IpsumORM"],
-  cloudAndDevOps: ["LoremCloud"],
+  backendDevelopment: ["Python", "NodeJS"],
+  databaseAndStorage: ["SQL", "MySQL"],
+  cloudAndDevOps: ["AWS, Ducker"],
   toolsAndServices: [
     "LoremAuth",
     "IpsumCMS",
@@ -95,7 +80,7 @@ export const skills = {
 
 export const projects = [
   {
-    title: "Lorem Ipsum Project",
+    title: "Proximamente",
     github: "https://github.com/loremipsum/project",
     description: [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -106,7 +91,7 @@ export const projects = [
     ],
   },
   {
-    title: "Dolor Sit Amet App",
+    title: "Proximamente",
     github: "https://github.com/loremipsum/dolorapp",
     description: [
       "Morbi in sem quis dui placerat ornare.",
